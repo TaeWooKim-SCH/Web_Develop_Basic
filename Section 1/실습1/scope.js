9435a494fe1e9932b56e1df48c9653a8131c8e22
TaeWooKim-SCH <79956107+TaeWooKim-SCH@users.noreply.github.com>
2023-03-02 14:25:26 +0900

    Add files via upload

---
+++ Section 1/실습1/scope.js
@@ -0,0 +1,8 @@
+function greetSomeone(firstName) {
+    var time = 'night';
+    if (time === 'night') {
+        var greeting = 'Good Night';
+    }
+    return greeting + ' ' + firstName;
+}
+greetSomeone('Steve');
