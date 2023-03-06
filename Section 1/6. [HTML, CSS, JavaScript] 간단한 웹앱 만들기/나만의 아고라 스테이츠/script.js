3f80700e01dc5d3961e2ebb72e14b98ffeef9cd6
TaeWooKim-SCH <79956107+TaeWooKim-SCH@users.noreply.github.com>
2023-03-06 17:53:53 +0900

    Add files via upload

--- Section 1/6. [HTML, CSS, JavaScript] 간단한 웹앱 만들기/나만의 아고라 스테이츠/script.js
+++ Section 1/6. [HTML, CSS, JavaScript] 간단한 웹앱 만들기/나만의 아고라 스테이츠/script.js
@@ -16,7 +16,7 @@ const convertToDiscussion = (obj) => {
   // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
     // img 넣기
   const avatarImg = document.createElement('img');
-  avatarImg.className = "discussion__avatar--image"
+  avatarImg.className = "discussion__avatar--image";
   avatarImg.src = obj.avatarUrl;
   avatarImg.alt = 'avatar of ' + obj.author;
   avatarWrapper.append(avatarImg);
@@ -32,29 +32,66 @@ const convertToDiscussion = (obj) => {
   discussion_h2.appendChild(discussion_link); // h2의 자식 요소로 a태그 추가
   discussionContent.append(discussion_h2); // 최종 추가
 
-    // 아이디/시간 넣기
+    // 작성자/시간 넣기
   const discussionInfo = document.createElement('div');
   discussionInfo.className = "discussion__information";
-  const infoContent = document.createTextNode(`${obj.id} / ${obj.createdAt}`)
+  const infoContent = document.createTextNode(`${obj.author} / ${obj.createdAt}`)
 
   discussionInfo.appendChild(infoContent);
   discussionContent.append(discussionInfo);
 
-    // 체크박스 넣기
-  const checkbox = document.createElement('div');
-  checkbox.className = "discussion__answered";
-  const check_p = document.createElement('p');
-  const check_text = document.createTextNode("☑");
+  //   // 체크박스 넣기
+  // const checkbox = document.createElement('div');
+  // checkbox.className = "discussion__answered";
+  // const check_p = document.createElement('p');
+  // const check_text = document.createTextNode("☑");
 
-  check_p.appendChild(check_text);
-  checkbox.appendChild(check_p);
-  discussionAnswered.append(checkbox);
+  // check_p.appendChild(check_text);
+  // checkbox.appendChild(check_p);
+  // discussionAnswered.append(checkbox);
 
+    // 질문 내용 넣기
+  const question = document.createElement('div');
+  question.className = "question_content";
 
+  discussionContent.appendChild(question);
+  question.innerHTML = obj.bodyHTML;
+
+    // 답변 내용 넣기
+    if (obj.answer !== undefined && obj.answer !== null) { // 답변이 있다면
+      // 답변 박스 선언
+      const answered = document.createElement('div');
+      answered.className = "discussion__content";
+      answered.id = "answerBox";
+      discussionContent.appendChild(answered);
+      
+      // 답변자 이미지
+      const answered_avatar_img = document.createElement('img');
+      answered_avatar_img.className = "answered_avatar_img";
+      answered_avatar_img.src = obj.answer.avatarUrl;
+
+      answered.appendChild(answered_avatar_img);
+      answered_avatar_img.innerHTML = answered_avatar_img;
+      
+      // 답변자명/시간 
+      const answered_info = document.createElement('div');
+      answered_info.className = "answered_info";
+
+      answered.appendChild(answered_info);
+      answered_info.innerHTML = `${obj.answer.author} / ${obj.answer.createdAt}`;
+
+      // 답변 내용
+      const answered_detail = document.createElement('div');
+      answered_detail.className = "answered_detail";
+      answered.appendChild(answered_detail);
+      answered_detail.innerHTML = obj.answer.bodyHTML;
+  }
 
   li.append(avatarWrapper, discussionContent, discussionAnswered);
   return li;
 };
+
+    
 
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
@@ -97,8 +134,17 @@ submit_btn.addEventListener("click", () => {
         "",
     }
     );
-  
-    document.querySelector('.discussions__container').prepend(li);
+    
+
+    if (name_value.trim() !== "" || title_value.trim() !== "" || question_value.trim() !== ""){
+      document.querySelector('.discussions__container').prepend(li);
+      document.querySelector('.form__input--name>input').value = "";
+      document.querySelector('.form__input--title>input').value = "";
+      document.querySelector('.form__textbox>textarea').value = "";
+    }
+    else {
+      alert("빈칸을 모두 입력해주세요.")
+    }
   }
 
 )
